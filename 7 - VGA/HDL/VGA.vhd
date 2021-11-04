7d2be0c311ba9caf55fea8a38332e0998ddc4442
lekgolo167 <167sonoma182@gmail.com>
2021-11-03 23:51:10 -0600

    basic VGA pattern

--- 7 - VGA/HDL/VGA.vhd
+++ 7 - VGA/HDL/VGA.vhd
@@ -22,63 +22,52 @@ architecture behave of VGA is
 	signal rstn_btn : std_logic;
 	signal next_btn : std_logic;
 	signal vga_clk : std_logic;
-	constant hs_count_max : natural := 800;
-	constant hs_porch_max : natural := 160;
-	signal hs_count : natural := 0;
-	constant vs_count_max : natural := 416800;
-	constant vs_porch_max : natural := 32800;
-	signal vs_count : natural := 0;
-	signal vs : std_logic;
+	signal vga_x : natural;
+	signal vga_y : natural;
 begin
 
 	p_hs_count : process (vga_clk)
 	begin
 		if rising_edge(vga_clk) then
-			if vs = '1' then
-				if hs_count >= hs_count_max then
-					hs_count <= 0;
-				elsif hs_count < hs_porch_max then
-					hs_count <= hs_count + 1;
-					VGA_HS <= '0';
-					VGA_R <= (others => '0');
-				else
-					hs_count <= hs_count + 1;
-					VGA_HS <= '1';
-					VGA_R <= (others => '1');
-				end if;
-			else
-				hs_count <= 0;
-				VGA_HS <= '0';
+			if vga_x < 200 then
+				VGA_R <= (others => '1');
+				VGA_B <= (others => '0');
+				VGA_G <= (others => '0');
+			elsif vga_x < 440 then
+				VGA_R <= (others => '1');
+				VGA_B <= (others => '1');
+				VGA_G <= (others => '1');
+			elsif vga_x < 640 then
 				VGA_R <= (others => '0');
-			end if;
-		end if;
-	end process;
-	
-	p_vs_count : process (vga_clk)
-	begin
-		if rising_edge(vga_clk) then
-			if vs_count >= vs_count_max then
-				vs_count <= 0;
-			elsif vs_count < vs_porch_max then
-				vs_count <= vs_count + 1;
-				vs <= '0';
+				VGA_B <= (others => '1');
+				VGA_G <= (others => '0');
 			else
-				vs_count <= vs_count + 1;
-				vs <= '1';
+				VGA_R <= (others => '0');
+				VGA_B <= (others => '0');
+				VGA_G <= (others => '0');
 			end if;
 		end if;
 	end process;
 	
 	rstn_btn <= KEY(0);
---	VGA_R <= (others => '0');
-	VGA_B <= (others => '0');
-	VGA_G <= (others => '0');
-	VGA_VS <= vs;
+	next_btn <= KEY(1);
+
+	VG0: entity work.VGA_Sync(behave)
+	port map (
+		vga_clk => vga_clk,
+		rstn => rstn_btn,
+		vga_hs => VGA_HS,
+		vga_x => vga_x,
+		vga_vs => VGA_VS,
+		vga_y => vga_y
+	);
+
 	PL0: entity work.PLL(syn)
-	port map	(
+	port map (
 		inclk0 => MAX10_CLK1_50,
 		c0	=> vga_clk
 	);
+
 end behave;
 
 
