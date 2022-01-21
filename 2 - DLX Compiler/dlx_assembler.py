ca0287a0d12fce5bf8dba3f63027d443d8cd193c
unknown <nicholascwilliams20@gmail.com>
2022-01-20 19:42:12 -0700

    Parser works

--- 2 - DLX Compiler/dlx_assembler.py
+++ 2 - DLX Compiler/dlx_assembler.py
@@ -5,16 +5,19 @@
 
 import re
 import argparse
+import sys
 
 from dlx_constants import *
 from dlx_utilities import *
 
-REG_DICT = {'R0':'00', 'R1':'01', 'R2':'10', 'R3':'11'}
-INST_DICT = {'LOAD':'00XXYYYY', 'STORE':'01XX0000', 'MOVE':'10XXYY00', 'ADD':'11XXYY00', 'SUB':'11XXYY01', 'AND':'11XXYY10', 'NOT':'11XXYY11'}
+REG_DICT = {'R0': '00', 'R1': '01', 'R2': '10', 'R3': '11'}
+INST_DICT = {'LOAD': '00XXYYYY', 'STORE': '01XX0000', 'MOVE': '10XXYY00',
+	'ADD': '11XXYY00', 'SUB': '11XXYY01', 'AND': '11XXYY10', 'NOT': '11XXYY11'}
 
 #######################################
 # POSITION
 #######################################
+
 
 class Position:
 	def __init__(self, idx, ln, col, fn, ftxt):
@@ -41,6 +44,7 @@ class Position:
 # TOKENS
 #######################################
 
+
 TT_INT = 'INT'
 TT_SEGMENT = 'SEGMENT'
 TT_INSTRUCTION = 'INSTRUCTION'
@@ -65,6 +69,7 @@ BIN_OPS = [
 DATA_OPS = [
 	'LOAD'
 ]
+
 
 class Token:
 	def __init__(self, type_, value=None, pos_start=None, pos_end=None):
@@ -91,6 +96,7 @@ class Token:
 # LEXER
 #######################################
 
+
 class Lexer:
 	def __init__(self, fn, text):
 		self.in_data_segment = False
@@ -110,7 +116,7 @@ class Lexer:
 		tokens = []
 
 		while self.current_char != None:
-			if self.current_char == ';': # skip comment lines
+			if self.current_char == ';':  # skip comment lines
 				while self.current_char != '\n':
 					self.advance()
 			elif self.current_char in ' (),\t\n':
@@ -151,7 +157,7 @@ class Lexer:
 		while self.current_char != None and self.current_char in LETTERS:
 			segment_name += self.current_char
 			self.advance()
-		
+
 		if segment_name == DATA_SEGMENT:
 			self.in_data_segment = True
 		else:
@@ -194,6 +200,7 @@ class Lexer:
 # PARSER
 #######################################
 
+
 class Parser:
 	def __init__(self, tokens):
 		self.tokens = tokens
@@ -209,48 +216,180 @@ class Parser:
 
 	def parse(self):
 		error = None
+		if self.current_tok.type == TT_SEGMENT and self.current_tok.value == DATA_SEGMENT:
+			self.advance()
+			while self.current_tok.type != TT_EOF and not error:
+				if self.current_tok.type == TT_SEGMENT:
+					if self.current_tok.value == TEXT_SEGMENT:
+						self.advance()
+						break
+					else:
+						print("ERROR")
+						break
+				self.advance()
 
 		while self.current_tok.type != TT_EOF and not error:
-
-			if self.current_tok.value in UNI_OPS:
-				error = self.bin_op(1, False)
-			elif self.current_tok.value in BIN_OPS:
-				error = self.bin_op(2, False)
-			elif self.current_tok.value in DATA_OPS:
-				error = self.bin_op(1, True)
+			if self.current_tok.value in REGISTER_OPS:
+				error = self.regs_op()
+			elif self.current_tok.value in IMMEDIATE_OPS:
+				error = self.imm_op()
+			elif self.current_tok.value in JUMP_OPS:
+				error = self.jump_op()
+			elif self.current_tok.value in BRANCH_OPS:
+				error = self.branch_op()
+			elif self.current_tok.value in MEMORY_OPS:
+				error = self.memory_op()
+			elif self.current_tok.value in NO_OPS:
+				error = self.no_op()
+			elif self.current_tok.type == TT_SEGMENT or self.current_tok.type == TT_LABEL:
+				self.advance()
 			else:
 				error = InvalidInstructionError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
 
 		return error
-
-	def bin_op(self, num, load_op):
+	
+	def regs_op(self):
 		inst = []
-
+		
 		inst.append(self.current_tok)
 		self.advance()
 		
-		for x in range(num):
-			if re.fullmatch(r'R\d', self.current_tok.value):
+		for i in range(3):
+			if self.current_tok.type == TT_REGISTER:
 				if self.current_tok.value in REGISTERS:
 					inst.append(self.current_tok)
 					self.advance()
 				else:
-					print(1)
 					return InvalidRegisterError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
 			else:
-				print(2)
-				return InvalidRegisterError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+				return ExpectedRegisterError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+		self.parsed_tokens.append(inst)
+		return None
+	
+	def imm_op(self):
+		inst = []
 		
-		if load_op:
-			if self.current_tok.value >= 0 and self.current_tok.value <= 15:
+		inst.append(self.current_tok)
+		self.advance()
+		
+		for i in range(2):
+			if self.current_tok.type == TT_REGISTER:
+				if self.current_tok.value in REGISTERS:
+					inst.append(self.current_tok)
+					self.advance()
+				else:
+					return InvalidRegisterError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+			else:
+				return ExpectedRegisterError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+		
+		if self.current_tok.type == TT_INT:
+			if self.current_tok.value < 0:
+				if int(self.current_tok.value) >= IMM_MIN_SIGNED:
+					inst.append(self.current_tok)
+				else:
+					return InvalidLoadValueError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+			else:
+				if int(self.current_tok.value) <= IMM_MAX_UNSIGNED:
+					inst.append(self.current_tok)
+				else:
+					return InvalidLoadValueError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+		else:
+			return ExpectedIntegerError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+		self.advance()
+		self.parsed_tokens.append(inst)
+		return None
+	
+	def jump_op(self):
+		inst = []
+		
+		inst.append(self.current_tok)
+		self.advance()
+		
+
+		if self.current_tok.type == TT_REGISTER:
+			if self.current_tok.value in REGISTERS:
+					inst.append(self.current_tok)
+					self.advance()
+			else:
+				return InvalidRegisterError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)      
+		elif self.current_tok.type == TT_LABEL:
+			inst.append(self.current_tok)
+			self.advance()
+		else:
+			return InvalidJumpOperandError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+		
+		self.parsed_tokens.append(inst)
+		return None
+		
+	
+	def branch_op(self):
+		inst = []
+		
+		inst.append(self.current_tok)
+		self.advance()  
+
+		if self.current_tok.type == TT_REGISTER:
+			if self.current_tok.value in REGISTERS:
+					inst.append(self.current_tok)
+					self.advance()
+			else:
+				return InvalidRegisterError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+		else:
+			return ExpectedRegisterError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+		
+		if self.current_tok.type == TT_LABEL:
+			inst.append(self.current_tok)
+			self.advance()
+		else:
+			return ExpectedLabelError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+		
+		self.parsed_tokens.append(inst)
+		return None
+	
+	def memory_op(self):
+		inst = [] 
+		
+		inst.append(self.current_tok)
+		
+		if self.current_tok.value == 'SW':
+			self.advance()
+			if self.current_tok.type == TT_VARIABLE:
 				inst.append(self.current_tok)
 				self.advance()
 			else:
-				return InvalidLoadValueError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
-
+				return ExpectedVariableError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+			if self.current_tok.type == TT_REGISTER:
+				inst.append(self.current_tok)
+				self.advance()
+			else:
+				return ExpectedRegisterError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+		else:
+			self.advance()
+			if self.current_tok.type == TT_REGISTER:
+				inst.append(self.current_tok)
+				self.advance()
+			else:
+				return ExpectedRegisterError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+			if self.current_tok.type == TT_VARIABLE:
+				inst.append(self.current_tok)
+				self.advance()
+			else:
+				return ExpectedVariableError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+		
+		if self.current_tok.type == TT_REGISTER:
+				inst.append(self.current_tok)
+				self.advance()
+		else:
+			return ExpectedRegisterError(self.current_tok.pos_start, self.current_tok.pos_end, self.current_tok.value)
+		
 		self.parsed_tokens.append(inst)
-
 		return None
+	
+	def no_op(self):
+		inst =[]
+		
+		inst.append(self.current_tok)
+		self.advance()
 
 #######################################
 # ASSEMBLER
@@ -300,40 +439,61 @@ def run(fn, text):
 	lexer = Lexer(fn, text)
 	tokens, error = lexer.make_tokens()
 	if error:
-		return None, error
-
-	# temporary delete this line after lexer works
-	return tokens, None
-
+		return None, None, error
+	print(tokens)
 	# Generate AST
 	parser = Parser(tokens)
 	error = parser.parse()
-	if error: return None, error
+	if error: return None, None, error
 	
+	print(parser.parsed_tokens)
+	data = 1
+	code = 1
 	# Generate verilog
-	assembler = Assembler()
-	assembler.build_binary(parser.parsed_tokens)
-	v = assembler.write_verilog()
-	return v, None
+# =============================================================================
+#     assembler = Assembler()
+#     assembler.build_binary(parser.parsed_tokens)
+#     v = assembler.write_verilog()
+# =============================================================================
+	return data, code, None
 
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser()
+# =============================================================================
+#     parser = argparse.ArgumentParser()
+# =============================================================================
 	
-	parser.add_argument('-i', type=str, required=True, help="file input name for assembly code")
-	parser.add_argument('-o', type=str, required=False, help="file output name for verilog code")
-	args = parser.parse_args()
+# =============================================================================
+#     parser.add_argument('-i', type=str, required=True, help="file input name for assembly code")
+#     parser.add_argument('-o', type=str, required=False, help="file output name for verilog code")
+#     args = parser.parse_args()
+# =============================================================================
+	datafile = None
+	codefile = None
+	if len(sys.argv) <= 1 or len(sys.argv) > 4 or len(sys.argv) == 3:
+		sys.exit("Error: invalid command line entry")
+	elif len(sys.argv) == 2:
+		infile = sys.argv[1]
+	else:
+		infile  = sys.argv[1]
+		datafile = sys.argv[2]
+		codefile = sys.argv[3]
+	
 
-	with open(args.i, 'r') as file:
+	with open(infile, 'r') as file:
 		text = ''.join(file.readlines())
-		result, error = run(args.i, text)
+		data, code, error = run(infile, text)
 
 		if error:
 			print(error.as_string())
 		else:
-			if args.o:
-				with open(args.o, 'w') as out:
-					out.writelines(result)
+			if datafile:
+				with open(datafile, 'w') as out:
+					out.writelines(data)
+			if codefile:
+				with open(codefile, 'w') as out:
+					out.writelines(code)
 			else:
-				print(result)
+				print(data)
+				print(code)
