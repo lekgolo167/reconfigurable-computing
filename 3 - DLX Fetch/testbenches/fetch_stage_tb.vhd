65061564353be2c100e4063bf149e98da3c3ea33
unknown <nicholascwilliams20@gmail.com>
2022-02-02 16:41:54 -0700

    made the testbench go through all of the loops and branches at least once

--- 3 - DLX Fetch/testbenches/fetch_stage_tb.vhd
+++ 3 - DLX Fetch/testbenches/fetch_stage_tb.vhd
@@ -19,6 +19,12 @@ architecture behave of fetch_stage_tb is
 	-- outputs
 	signal pc_counter : std_logic_vector(c_DLX_PC_WIDTH-1 downto 0);
 	signal instruction : std_logic_vector(c_DLX_WORD_WIDTH-1 downto 0);
+	-- variables
+	signal loop_counter : natural := 0;
+	signal func_counter : natural := 0;
+	signal took_jal : std_logic := '0';
+	signal took_bnez : std_logic := '0';
+	signal jal_counter : natural := 0;
 
 begin
 	-- generate clock
@@ -36,14 +42,35 @@ begin
 			instruction => instruction
 		);
 			
+		-- Compute 3 factorial
 		p_TEST : process(clk)
 		begin
 		if rising_edge(clk) then
-			if instruction(31 downto 26) = "101101" or instruction(31 downto 26) = "101011" or instruction(31 downto 26) = "101100" then
+			if instruction(31 downto 26) = "101101" and took_jal = '0' then  -- if instruction == J
 				jump_addr(9 downto 0) <= instruction(9 downto 0);
 				branch_taken <= '1';
+			elsif (instruction(31 downto 26) = "101100") and (loop_counter < 1) then  -- else if instruction == BNEZ
+				jump_addr(9 downto 0) <= instruction(9 downto 0);
+				branch_taken <= '1';
+				took_bnez <= '1';
+				loop_counter <= loop_counter + 1;
+			elsif (instruction(31 downto 26) = "101011") and (func_counter >= 1) then  -- else if instruction == BEQZ
+				jump_addr(9 downto 0) <= instruction(9 downto 0);
+				branch_taken <= '1';
+			elsif instruction(31 downto 26) = "101111" and (jal_counter < 1) then  -- else if instruction == JAL
+				jump_addr(9 downto 0) <= instruction(9 downto 0);
+				branch_taken <= '1';
+				func_counter <= func_counter + 1;
+				took_jal <= '1';
+				jal_counter <= 1;
+			elsif instruction(31 downto 26) = "101110" and took_bnez = '0' then  -- else if instruction == JR
+				jump_addr(9 downto 0) <= "0000000011";  -- jump back to address 0x005
+				branch_taken <= '1';
+				loop_counter <= 0;
 			else
 				branch_taken <= '0';
+				took_jal <= '0';
+				took_bnez <= '0';
 			end if;
 		end if;
 		end process;
